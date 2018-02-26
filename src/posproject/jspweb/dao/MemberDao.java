b89002b89f83f09b9075ded3a54fda10544c9a85
85 <85@DESKTOP-1V8JKNB>
2018-02-26 09:05:04 +0900

    성래꺼

---
+++ src/posproject/jspweb/dao/MemberDao.java
@@ -0,0 +1,15 @@
+package posproject.jspweb.dao;
+
+import java.util.List;
+
+import posproject.jspweb.entity.Member;
+
+public interface MemberDao {
+
+	int insert (Member member);
+	int delete(String id);
+	int update(Member member);
+	
+	Member get(String id);
+	List<Member> getList(); 
+}
---
+++ src/posproject/jspweb/entity/Member.java
@@ -0,0 +1,71 @@
+package posproject.jspweb.entity;
+
+public class Member {
+	private String id;
+	private String name; 
+	private String presentCoupon; 
+	private String phoneNumber; 
+	private String rankId; 
+	private String memberId;
+	
+	public Member(String id,String name,String presentCoupon,String phoneNumber,String rankId,String memberId ) {
+		this.id=id;
+		this.name=name;
+		this.presentCoupon=presentCoupon;
+		this.phoneNumber=phoneNumber;
+		this.rankId=rankId;
+		this.memberId=memberId;
+		
+		
+	}
+	
+	public Member(){
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPresentCoupon() {
+		return presentCoupon;
+	}
+	public void setPresentCoupon(String presentCoupon) {
+		this.presentCoupon = presentCoupon;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getRankId() {
+		return rankId;
+	}
+	public void setRankId(String rankId) {
+		this.rankId = rankId;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
+	
+	
+	
+	
+
+
+	
+	
+}
