af05d13a57a1a119fb42f23661039b5f266d2d7e
85 <85@DESKTOP-1V8JKNB>
2018-03-08 17:44:43 +0900

    유림아 하이

--- src/posproject/jspweb/entity/Member.java
+++ src/posproject/jspweb/entity/Member.java
@@ -8,21 +8,29 @@ public class Member {
 	private String rankId; 
 	private String memberId;
 	
-	public Member(String id,String name,String presentCoupon,String phoneNumber,String rankId,String memberId ) {
-		this.id=id;
-		this.name=name;
-		this.presentCoupon=presentCoupon;
-		this.phoneNumber=phoneNumber;
-		this.rankId=rankId;
-		this.memberId=memberId;
-		
+	
+	//------------------------생성자
+	
+	
+	// ----------기본 생성자
+	public Member() {
 		
 	}
 	
-	public Member(){
-		// TODO Auto-generated constructor stub
+	//----------변수 들어간 생성자
+	public Member( String id, String name, String presentCoupon, String phoneNumber, String rankId, String memberId ) {
+		this.id = id;
+		this.name = name;
+		this.presentCoupon = presentCoupon;
+		this.phoneNumber = phoneNumber;
+		this.rankId = rankId;
+		this.memberId = memberId;		
 	}
 	
+	
+	
+	
+	//--------------------------GETTER SETTER
 	public String getId() {
 		return id;
 	}
@@ -61,63 +69,6 @@ public class Member {
 	}
 	
 	
-	
-	
-	
-
-
-=======
-	헷갈리네...
-	private String id;
-	private String name;
-	private int presentCoupon;
-	private String tel;
-	private String rankId;
-	private String memberId;
-	
-	
-	
-	
-	
-	
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPresentCoupon() {
-		return presentCoupon;
-	}
-	public void setPresentCoupon(int presentCoupon) {
-		this.presentCoupon = presentCoupon;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getRankId() {
-		return rankId;
-	}
-	public void setRankId(String rankId) {
-		this.rankId = rankId;
-	}
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-
 	
 	
 }
